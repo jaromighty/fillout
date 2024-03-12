07caf2ba783990f081b465c474770ab9b9b27244
Jarom Rial <jarom@firebirdtech.dev>
2024-03-12 13:42:44 -0600

    add filtering for equals

--- src/index.js
+++ src/index.js
@@ -13,12 +13,37 @@ const headers = {
 app.get('/:formId/filteredResponses', async (req, res) => {
   const formId = req.params['formId']
   try {
-    const filloutResponse = await axios.get(`${baseEndpoint}/${formId}/submissions`, {
+    let filloutResponse = await axios.get(`${baseEndpoint}/${formId}/submissions`, {
       params: req.query,
       headers
     })
-    console.log(filloutResponse.data)
-    res.send('Hello World!')
+
+    let responseData = filloutResponse.data
+    let filteredResponses = responseData.responses
+    
+    if (req.query['filters']?.length > 0) {
+      let parsedFilters = JSON.parse(req.query['filters'])
+      let responsesToRemove = []
+      parsedFilters.forEach((filter) => {
+        filteredResponses.forEach((response, responseIdx) => {
+          let question = response.questions.find((question) => question.id === filter.id)
+          if (question.value !== filter.value) {
+            responsesToRemove.push(responseIdx)
+          }
+        })
+        console.log(responsesToRemove)
+        filteredResponses = filteredResponses.filter((response, index) => !responsesToRemove.includes(index))
+        console.log(filteredResponses)
+      })
+    } else {
+      res.send('No responses to display')
+    }
+    responseData = {
+      ...responseData,
+      responses: filteredResponses
+    }
+    res.send(responseData)
+    // console.log(responses)
   } catch (err) {
     console.log(err)
     res.status(500).send('Something went wrong')
